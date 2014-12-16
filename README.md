76d26e34fed591bb534ae716105e3816e03cae62
Natanael Arndt <arndtn@gmail.com>
2014-12-16 11:10:38 +0100

    Transform dates into virtuoso format and update README

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This collection of scripts can be used to import and backup models managed in vi
 For each model you have to maintain a `model.ttl` file and a`model.ttl.graph` file, which contains the graph URI.
 
 You have to define the `dump_one_graph` command in virtuoso before  [Bulk Loading RDF Source Files into one or more Graph IRIs](http://virtuoso.openlinksw.com/dataspace/doc/dav/wiki/Main/VirtBulkRDFLoader).
+
+The `normalize.sh` script is for sorting the triples and transforming them into a format that after running the `backup.sh` the resulting diff will be as minimal as possible.
--- normalize.sh
+++ normalize.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 ttl_file=$1
+file_name=${ttl_file%.ttl}
+tmp_nt='tmp.nt'
 
-cp prefixes.ttl ${ttl_file%.ttl}.nt
-rapper -q -i turtle -o ntriples ${ttl_file} | sort -u >> ${ttl_file%.ttl}.nt
-rapper -q -i turtle -o turtle ${ttl_file%.ttl}.nt > ${ttl_file}
+cp prefixes.ttl ${tmp_nt}
+rapper -q -i turtle -o ntriples ${ttl_file} | sort -u >> ${tmp_nt}
+sed \
+    -e 's/"\([0-9]\{4\}\)"^^<http:\/\/www.w3.org\/2001\/XMLSchema#gYear>/"\1-01-01T00:00:00Z"^^<http:\/\/www.w3.org\/2001\/XMLSchema#gYear>/g' \
+    -e 's/"\([0-9]\{4\}-[0-9]\{2\}\)"^^<http:\/\/www.w3.org\/2001\/XMLSchema#gYearMonth>/"\1-01T00:00:00Z"^^<http:\/\/www.w3.org\/2001\/XMLSchema#gYearMonth>/g' \
+    -e 's/"\([0-9]\{2\}-[0-9]\{2\}\)"^^<http:\/\/www.w3.org\/2001\/XMLSchema#gMonthDay>/"0001-\1T00:00:00Z"^^<http:\/\/www.w3.org\/2001\/XMLSchema#gMonthDay>/g' \
+    ${tmp_nt} > ${file_name}.nt
+rm ${tmp_nt}
+rapper -q -i turtle -o turtle ${file_name}.nt > ${ttl_file}
